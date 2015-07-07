553e593ac2b8643103fd47bcb802ced5e36a88ee
unknown <taokai@pc-taokai.hikvision.com>
2015-07-07 16:46:19 +0800

    bugfix:unbind the button which should be used only once

--- AntSystem/js/action.js
+++ AntSystem/js/action.js
@@ -19,13 +19,14 @@ $(document).ready(function(){
 	 // [last,lastRoute]=AntSystem(C);
 
 	 $(".getNum").click(function(){
-	  	antNum=parseInt($(".num").val());
+	 	antNum=parseInt($(".num").val());
 	 	if(antNum!=antNum){
 	 		$(".state").text("输入错误！不是一个数字!");
 	 	}else if(antNum<3||antNum>100){
 	 		$(".state").text("输入错误！n的大小不满足条件!");
 	 	}else{
 	 		$(".state").text("输入正确！请继续输入城市坐标矩阵!");
+	 		$(".getNum").unbind('click'); //解除click绑定
 	 		setTimeout(function(){
 	 			$(".dataInput").fadeIn();
 	 			var temp="<li><input type='number' class='martrixData' name='martrixData' /><input type='number' class='martrixData' name='martrixData' /></li>";
@@ -34,9 +35,9 @@ $(document).ready(function(){
 	 			}
 	 		},100);
 	 	}
-	 });
+	 });  //由于这里有对输入数值的判断，因此不能简单的执行一次，而是在输入正确的时候去掉click事件绑定。
 
-	 $(".getMartrix").click(function(){
+	 $(".getMartrix").one("click",function(){   //仅执行一次，因此执行完后解除事件
 	 	$(".dataOutput").fadeIn();
 	 	var C;
 	 	C=new2Arr(C,antNum);
